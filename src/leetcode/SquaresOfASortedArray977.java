48e8bda507e7ef8f1ed491986ca5c8006236ce7d
nigarlyva12 <nigarlyva12@gmail.com>
2026-09-03 20:24:47 +0200

    leetcode suqare of a sorted array version 2

--- src/leetcode/SquaresOfASortedArray977.java
+++ src/leetcode/SquaresOfASortedArray977.java
@@ -9,7 +9,7 @@ public class SquaresOfASortedArray977 {
 			System.out.print(el + " ");
 		}
 	}
-	
+	//version 1
 	public static int[] sortedSquares(int[] nums) {
 		for(int i=0; i<nums.length; i++) {
 			nums[i] = (int) Math.pow(nums[i], 2);
@@ -24,5 +24,28 @@ public class SquaresOfASortedArray977 {
 			}
 		}
 		return nums;
+	}
+	//version 2 O(n) time complexity
+	
+	public static int[] sortedSquaresV2(int[] nums) {
+		int left = 0;
+		int right = nums.length-1;
+		int[] result = new int[nums.length];
+		int pos = nums.length-1;
+		
+		while(left<=right) {
+			int leftSquare = nums[left]*nums[left];
+			int rightSquare = nums[right] * nums[right];
+			
+			if(leftSquare>rightSquare) {
+				result[pos] = leftSquare;
+				left++;
+			}else {
+				result[pos]=rightSquare;
+				right--;
+			}
+			pos--;
+		}
+		return result;
 	}
 }
