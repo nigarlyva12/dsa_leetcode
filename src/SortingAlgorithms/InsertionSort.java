301c9277efa37875b5c793cd38f4e02af12d3210
nigarlyva12 <nigarlyva12@gmail.com>
2026-09-03 19:29:14 +0200

    Insertion Sorting

---
+++ src/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,20 @@
+package SortingAlgorithms;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		
+	}
+	public static void insertionSort(int[] arr) {
+		for(int i=1; i<arr.length; i++) {
+			int current = arr[i];
+			int j=i-1;
+			
+			while(j>=0 && arr[j]>current) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			
+			arr[j+1] = current;
+		}
+	}
+}
